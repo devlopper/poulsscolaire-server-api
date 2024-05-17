dd7dc658f7bc3c608909062dae253f704a07992a
christian <kycdev@gmail.com>
2024-05-17 23:04:31 +0000

    refactoring

--- src/main/java/org/cyk/system/poulsscolaire/server/api/fee/FeeCategoryDto.java
+++ src/main/java/org/cyk/system/poulsscolaire/server/api/fee/FeeCategoryDto.java
@@ -10,4 +10,13 @@ import ci.gouv.dgbf.extension.server.service.api.entity.AbstractIdentifiableCoda
  */
 public class FeeCategoryDto extends AbstractIdentifiableCodableNamableAuditableDto {
 
+  /**
+   * Nom pour lecture humaine de la classe.
+   */
+  public static final String NAME = "rubrique";
+  
+  /**
+   * Nom au pluriel pour lecture humaine de la classe.
+   */
+  public static final String PLURAL_NAME = NAME + "s";
 }
