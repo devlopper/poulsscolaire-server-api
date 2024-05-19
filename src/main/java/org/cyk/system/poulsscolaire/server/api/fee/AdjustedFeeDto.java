e629f3874171cea62a9f545770261584fd707b56
christian <kycdev@gmail.com>
2024-05-19 13:36:03 +0000

    refactoring

--- src/main/java/org/cyk/system/poulsscolaire/server/api/fee/AdjustedFeeDto.java
+++ src/main/java/org/cyk/system/poulsscolaire/server/api/fee/AdjustedFeeDto.java
@@ -18,41 +18,53 @@ public class AdjustedFeeDto extends AbstractAmountContainerDto {
 
   @JsonbProperty(JSON_FEE_IDENTIFIER)
   private String feeIdentifier;
-  
+
   @JsonbProperty(JSON_FEE_AS_STRING)
   private String feeAsString;
-  
+
   @JsonbProperty(JSON_REGISTRATION_IDENTIFIER)
   private String registrationIdentifier;
-  
+
   @JsonbProperty(JSON_REGISTRATION_AS_STRING)
   private String registrationAsString;
-  
+
   @JsonbProperty(JSON_AMOUNT_VALUE_PAID)
   private Integer amountValuePaid;
 
   @JsonbProperty(JSON_AMOUNT_VALUE_PAID_AS_STRING)
   private String amountValuePaidAsString;
-  
+
+  @JsonbProperty(JSON_AMOUNT_VALUE_LEFT_TO_PAY)
+  private Integer amountValueLeftToPay;
+
+  @JsonbProperty(JSON_AMOUNT_VALUE_LEFT_TO_PAY_AS_STRING)
+  private String amountValueLeftToPayAsString;
+
   public static final String FIELD_FEE_IDENTIFIER = "feeIdentifier";
   public static final String FIELD_FEE_AS_STRING = "feeAsString";
   public static final String FIELD_REGISTRATION_IDENTIFIER = "registrationIdentifier";
   public static final String FIELD_REGISTRATION_AS_STRING = "registrationAsString";
   public static final String FIELD_AMOUNT_VALUE_PAID = "amountValuePaid";
   public static final String FIELD_AMOUNT_VALUE_PAID_AS_STRING = "amountValuePaidAsString";
-  
+  public static final String FIELD_AMOUNT_VALUE_LEFT_TO_PAY = "amountValueLeftToPay";
+  public static final String FIELD_AMOUNT_VALUE_LEFT_TO_PAY_AS_STRING =
+      "amountValueLeftToPayAsString";
+
   public static final String JSON_FEE_IDENTIFIER = "idFrais";
   public static final String JSON_FEE_AS_STRING = "fraisChaine";
   public static final String JSON_REGISTRATION_IDENTIFIER = "idInscription";
   public static final String JSON_REGISTRATION_AS_STRING = "inscriptionChaine";
   public static final String JSON_AMOUNT_VALUE_PAID = "valeurPayeMontant";
   public static final String JSON_AMOUNT_VALUE_PAID_AS_STRING = "valeurPayeMontantChaine";
-  
+  public static final String JSON_AMOUNT_VALUE_LEFT_TO_PAY = "valeurResteAPayerMontant";
+  public static final String JSON_AMOUNT_VALUE_LEFT_TO_PAY_AS_STRING =
+      "valeurResteAPayerMontantChaine";
+
   /**
    * Nom pour lecture humaine de la classe.
    */
   public static final String NAME = "frais ajusté";
-  
+
   /**
    * Nom au pluriel pour lecture humaine de la classe.
    */
