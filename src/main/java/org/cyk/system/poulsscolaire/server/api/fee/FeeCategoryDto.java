5df42f74600c955ed6f99ad8cdff158a1b9b56f9
Christian Yao Komenan <c.komenan@budget.gouv.ci>
2024-06-02 22:59:06 +0000

    refactoring

--- src/main/java/org/cyk/system/poulsscolaire/server/api/fee/FeeCategoryDto.java
+++ src/main/java/org/cyk/system/poulsscolaire/server/api/fee/FeeCategoryDto.java
@@ -1,6 +1,10 @@
 package org.cyk.system.poulsscolaire.server.api.fee;
 
 import ci.gouv.dgbf.extension.server.service.api.entity.AbstractIdentifiableCodableNamableAuditableDto;
+import jakarta.json.bind.annotation.JsonbProperty;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Cette classe représente une rubrique.
@@ -8,8 +12,57 @@ import ci.gouv.dgbf.extension.server.service.api.entity.AbstractIdentifiableCoda
  * @author Christian
  *
  */
+@Getter
+@Setter
+@EqualsAndHashCode(callSuper = false)
 public class FeeCategoryDto extends AbstractIdentifiableCodableNamableAuditableDto {
 
+  /*
+   * Amount
+   */
+  @JsonbProperty(JSON_TOTAL_AMOUNT_AS_STRING)
+  private String totalAmountAsString;
+
+  @JsonbProperty(JSON_PAID_AMOUNT_AS_STRING)
+  private String paidAmountAsString;
+
+  @JsonbProperty(JSON_PAYABLE_AMOUNT_AS_STRING)
+  private String payableAmountAsString;
+
+  /*
+   * Registration amount
+   */
+  @JsonbProperty(JSON_TOTAL_REGISTRATION_AMOUNT_AS_STRING)
+  private String totalRegistrationAmountAsString;
+
+  @JsonbProperty(JSON_PAID_REGISTRATION_AMOUNT_AS_STRING)
+  private String paidRegistrationAmountAsString;
+
+  @JsonbProperty(JSON_PAYABLE_REGISTRATION_AMOUNT_AS_STRING)
+  private String payableRegistrationAmountAsString;
+  
+  public static final String FIELD_TOTAL_AMOUNT_AS_STRING = "totalAmountAsString";
+  public static final String FIELD_PAID_AMOUNT_AS_STRING = "paidAmountAsString";
+  public static final String FIELD_PAYABLE_AMOUNT_AS_STRING = "payableAmountAsString";
+
+  public static final String FIELD_TOTAL_REGISTRATION_AMOUNT_AS_STRING =
+      "totalRegistrationAmountAsString";
+  public static final String FIELD_PAID_REGISTRATION_AMOUNT_AS_STRING =
+      "paidRegistrationAmountAsString";
+  public static final String FIELD_PAYABLE_REGISTRATION_AMOUNT_AS_STRING =
+      "payableRegistrationAmountAsString";
+  
+  public static final String JSON_TOTAL_AMOUNT_AS_STRING = "montantTotalChaine";
+  public static final String JSON_PAID_AMOUNT_AS_STRING = "montantPayeChaine";
+  public static final String JSON_PAYABLE_AMOUNT_AS_STRING = "montantPayableChaine";
+
+  public static final String JSON_TOTAL_REGISTRATION_AMOUNT_AS_STRING =
+      "montantInscriptionTotalChaine";
+  public static final String JSON_PAID_REGISTRATION_AMOUNT_AS_STRING =
+      "montantInscriptionPayeChaine";
+  public static final String JSON_PAYABLE_REGISTRATION_AMOUNT_AS_STRING =
+      "montantInscriptionPayableChaine";
+  
   /**
    * Nom pour lecture humaine de la classe.
    */
--- src/main/java/org/cyk/system/poulsscolaire/server/api/fee/FeeDto.java
+++ src/main/java/org/cyk/system/poulsscolaire/server/api/fee/FeeDto.java
@@ -46,30 +46,6 @@ public class FeeDto extends AbstractAmountContainerDto {
   @JsonbProperty(JSON_SCHOOLING_BRANCH_AS_STRING)
   private String schoolingBranchAsString;
 
-  /*
-   * Amount
-   */
-  @JsonbProperty(JSON_TOTAL_AMOUNT_AS_STRING)
-  private String totalAmountAsString;
-
-  @JsonbProperty(JSON_PAID_AMOUNT_AS_STRING)
-  private String paidAmountAsString;
-
-  @JsonbProperty(JSON_PAYABLE_AMOUNT_AS_STRING)
-  private String payableAmountAsString;
-
-  /*
-   * Registration amount
-   */
-  @JsonbProperty(JSON_TOTAL_REGISTRATION_AMOUNT_AS_STRING)
-  private String totalRegistrationAmountAsString;
-
-  @JsonbProperty(JSON_PAID_REGISTRATION_AMOUNT_AS_STRING)
-  private String paidRegistrationAmountAsString;
-
-  @JsonbProperty(JSON_PAYABLE_REGISTRATION_AMOUNT_AS_STRING)
-  private String payableRegistrationAmountAsString;
-
   public static final String FIELD_CATEGORY_IDENTIFIER = "categoryIdentifier";
   public static final String FIELD_CATEGORY_AS_STRING = "categoryAsString";
   public static final String FIELD_ASSIGNMENT_IDENTIFIER = "assignmentTypeIdentifier";
@@ -78,16 +54,6 @@ public class FeeDto extends AbstractAmountContainerDto {
   public static final String FIELD_SENIORITY_AS_STRING = "seniorityAsString";
   public static final String FIELD_SCHOOLING_IDENTIFIER = "schoolingIdentifier";
   public static final String FIELD_SCHOOLING_AS_STRING = "schoolingAsString";
-  public static final String FIELD_TOTAL_AMOUNT_AS_STRING = "totalAmountAsString";
-  public static final String FIELD_PAID_AMOUNT_AS_STRING = "paidAmountAsString";
-  public static final String FIELD_PAYABLE_AMOUNT_AS_STRING = "payableAmountAsString";
-
-  public static final String FIELD_TOTAL_REGISTRATION_AMOUNT_AS_STRING =
-      "totalRegistrationAmountAsString";
-  public static final String FIELD_PAID_REGISTRATION_AMOUNT_AS_STRING =
-      "paidRegistrationAmountAsString";
-  public static final String FIELD_PAYABLE_REGISTRATION_AMOUNT_AS_STRING =
-      "payableRegistrationAmountAsString";
 
   public static final String JSON_CATEGORY_IDENTIFIER = "idCategorie";
   public static final String JSON_CATEGORY_AS_STRING = "categorieChaine";
@@ -99,16 +65,6 @@ public class FeeDto extends AbstractAmountContainerDto {
   public static final String JSON_SCHOOLING_SCHOOL_AS_STRING = "ecoleScolariteChaine";
   public static final String JSON_SCHOOLING_PERIOD_AS_STRING = "periodeScolariteChaine";
   public static final String JSON_SCHOOLING_BRANCH_AS_STRING = "brancheScolariteChaine";
-  public static final String JSON_TOTAL_AMOUNT_AS_STRING = "montantTotalChaine";
-  public static final String JSON_PAID_AMOUNT_AS_STRING = "montantPayeChaine";
-  public static final String JSON_PAYABLE_AMOUNT_AS_STRING = "montantPayableChaine";
-
-  public static final String JSON_TOTAL_REGISTRATION_AMOUNT_AS_STRING =
-      "montantInscriptionTotalChaine";
-  public static final String JSON_PAID_REGISTRATION_AMOUNT_AS_STRING =
-      "montantInscriptionPayeChaine";
-  public static final String JSON_PAYABLE_REGISTRATION_AMOUNT_AS_STRING =
-      "montantInscriptionPayableChaine";
 
   /**
    * Nom pour lecture humaine de la classe.
