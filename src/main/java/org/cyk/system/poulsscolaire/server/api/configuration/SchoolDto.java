4264bb8cb1fa64738df528d105bb477b917837c0
Christian Yao Komenan <c.komenan@budget.gouv.ci>
2024-06-30 22:45:54 +0000

    refactoring

--- src/main/java/org/cyk/system/poulsscolaire/server/api/configuration/SchoolDto.java
+++ src/main/java/org/cyk/system/poulsscolaire/server/api/configuration/SchoolDto.java
@@ -1,6 +1,7 @@
 package org.cyk.system.poulsscolaire.server.api.configuration;
 
 import ci.gouv.dgbf.extension.server.service.api.entity.AbstractIdentifiableNamableDto;
+import jakarta.json.bind.annotation.JsonbProperty;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,6 +16,41 @@ import lombok.Setter;
 @Setter
 @EqualsAndHashCode(callSuper = false)
 public class SchoolDto extends AbstractIdentifiableNamableDto {
+  
+  /*
+   * Amount
+   */
+  @JsonbProperty(JSON_TOTAL_AMOUNT_AS_STRING)
+  private String totalAmountAsString;
+
+  @JsonbProperty(JSON_PAID_AMOUNT_AS_STRING)
+  private String paidAmountAsString;
+
+  @JsonbProperty(JSON_PAYABLE_AMOUNT_AS_STRING)
+  private String payableAmountAsString;
+
+  /*
+   * Registration amount
+   */
+  @JsonbProperty(JSON_TOTAL_REGISTRATION_AMOUNT_AS_STRING)
+  private String totalRegistrationAmountAsString;
+
+  @JsonbProperty(JSON_PAID_REGISTRATION_AMOUNT_AS_STRING)
+  private String paidRegistrationAmountAsString;
+
+  @JsonbProperty(JSON_PAYABLE_REGISTRATION_AMOUNT_AS_STRING)
+  private String payableRegistrationAmountAsString;
+
+  public static final String JSON_TOTAL_AMOUNT_AS_STRING = "montantTotalChaine";
+  public static final String JSON_PAID_AMOUNT_AS_STRING = "montantPayeChaine";
+  public static final String JSON_PAYABLE_AMOUNT_AS_STRING = "montantPayableChaine";
+
+  public static final String JSON_TOTAL_REGISTRATION_AMOUNT_AS_STRING =
+      "montantInscriptionTotalChaine";
+  public static final String JSON_PAID_REGISTRATION_AMOUNT_AS_STRING =
+      "montantInscriptionPayeChaine";
+  public static final String JSON_PAYABLE_REGISTRATION_AMOUNT_AS_STRING =
+      "montantInscriptionPayableChaine";
   
   /**
    * Nom pour lecture humaine de la classe.
