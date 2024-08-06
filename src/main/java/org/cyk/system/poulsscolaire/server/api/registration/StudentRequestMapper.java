9a722e1106785b309e037ca158306dbb5eccd67d
Christian Yao Komenan <c.komenan@budget.gouv.ci>
2024-08-06 09:05:36 +0000

    refactoring

---
+++ src/main/java/org/cyk/system/poulsscolaire/server/api/registration/StudentRequestMapper.java
@@ -0,0 +1,22 @@
+package org.cyk.system.poulsscolaire.server.api.registration;
+
+import org.cyk.system.poulsscolaire.server.api.registration.StudentService.StudentCreateRequestDto;
+import org.cyk.system.poulsscolaire.server.api.registration.StudentService.StudentUpdateRequestDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+/**
+ * Cette interface représente le mapping de requête et {@link StudentDto}.
+ *
+ * @author Christian
+ *
+ */
+@Mapper
+public interface StudentRequestMapper {
+
+  StudentRequestMapper INSTANCE = Mappers.getMapper(StudentRequestMapper.class);
+
+  StudentCreateRequestDto mapCreate(StudentDto identity);
+
+  StudentUpdateRequestDto mapUpdate(StudentDto identity);
+}
---
+++ src/test/java/org/cyk/system/poulsscolaire/server/api/registration/StudentRequestMapperTest.java
@@ -0,0 +1,32 @@
+package org.cyk.system.poulsscolaire.server.api.registration;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.Test;
+
+class StudentRequestMapperTest {
+
+  StudentRequestMapper mapper = StudentRequestMapper.INSTANCE;
+  
+  @Test
+  void mapCreation_whenNull() {
+    assertNull(mapper.mapCreate(null));
+  }
+  
+  @Test
+  void mapCreation_whenNotNull() {
+    assertNotNull(mapper.mapCreate(new StudentDto()));
+  }
+  
+  @Test
+  void mapUpdate_whenNull() {
+    assertNull(mapper.mapUpdate(null));
+  }
+  
+  @Test
+  void mapUpdate_whenNotNull() {
+    assertNotNull(mapper.mapUpdate(new StudentDto()));
+  }
+  
+}
