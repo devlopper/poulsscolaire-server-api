d0670c7acf13bea47e4e5bdb9fd3f93ff89ead0d
Christian Yao Komenan <c.komenan@budget.gouv.ci>
2024-09-25 22:29:49 +0000

    refactoring

--- src/main/java/org/cyk/system/poulsscolaire/server/api/accounting/AccountingAccountFilter.java
+++ src/main/java/org/cyk/system/poulsscolaire/server/api/accounting/AccountingAccountFilter.java
@@ -14,6 +14,9 @@ import lombok.Setter;
 @Getter
 @Setter
 public class AccountingAccountFilter extends AbstractIdentifiableFilter {
+
+  AccountingAccountType type;
+
   public AccountingAccountFilter(FilterDto dto) {
     super(dto);
   }
@@ -23,5 +26,34 @@ public class AccountingAccountFilter extends AbstractIdentifiableFilter {
   @Override
   protected void doInitialize(FilterDto filter) {
     super.doInitialize(filter);
+    type = getType(filter);
   }
+
+  @Override
+  protected void toDto(FilterDto filter) {
+    super.toDto(filter);
+    setType(filter, type);
+  }
+
+  /**
+   * Cette méthode permet d'assigner la {@link AccountingAccountType}.
+   *
+   * @param filter filtre
+   * @param type {@link AccountingAccountType}
+   */
+  public static void setType(FilterDto filter, AccountingAccountType type) {
+    set(filter, JSON_TYPE, f -> f.getValueAsString(), f -> f.setValueAsEnum(type));
+  }
+
+  /**
+   * Cette méthode permet d'obtenir l'identifiant de {@link AccountingAccountType}.
+   *
+   * @param filter filtre
+   * @return {@link AccountingAccountType}
+   */
+  public static AccountingAccountType getType(FilterDto filter) {
+    return get(filter, d -> d.getFieldValueAsEnum(JSON_TYPE, AccountingAccountType.class));
+  }
+
+  public static final String JSON_TYPE = AccountingAccountDto.JSON_TYPE;
 }
