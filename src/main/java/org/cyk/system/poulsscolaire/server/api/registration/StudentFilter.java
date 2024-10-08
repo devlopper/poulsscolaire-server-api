d944a1600db4613bf23091e3e6428e2d4c2473a8
Christian Yao Komenan <c.komenan@budget.gouv.ci>
2024-10-08 12:41:30 +0000

    refactoring

--- src/main/java/org/cyk/system/poulsscolaire/server/api/registration/StudentFilter.java
+++ src/main/java/org/cyk/system/poulsscolaire/server/api/registration/StudentFilter.java
@@ -14,9 +14,14 @@ import lombok.Setter;
 @Getter
 @Setter
 public class StudentFilter extends AbstractIdentifiableFilter {
-  
+
   String schoolIdentifier;
-  
+  String registrationNumber;
+  String firstName;
+  String lastNames;
+  String arabicFirstName;
+  String arabicLastNames;
+
   public StudentFilter(FilterDto dto) {
     super(dto);
   }
@@ -27,12 +32,22 @@ public class StudentFilter extends AbstractIdentifiableFilter {
   protected void doInitialize(FilterDto filter) {
     super.doInitialize(filter);
     schoolIdentifier = getSchoolIdentifier(filter);
+    registrationNumber = getRegistrationNumber(filter);
+    firstName = getFirstName(filter);
+    lastNames = getLastNames(filter);
+    arabicFirstName = getArabicFirstName(filter);
+    arabicLastNames = getArabicLastNames(filter);
   }
 
   @Override
   protected void toDto(FilterDto filter) {
     super.toDto(filter);
     setSchoolIdentifier(filter, schoolIdentifier);
+    setRegistrationNumber(filter, registrationNumber);
+    setFirstName(filter, firstName);
+    setLastNames(filter, lastNames);
+    setArabicFirstName(filter, arabicFirstName);
+    setArabicLastNames(filter, arabicLastNames);
   }
 
   public static void setSchoolIdentifier(FilterDto filter, String identifier) {
@@ -44,5 +59,53 @@ public class StudentFilter extends AbstractIdentifiableFilter {
     return get(filter, d -> d.getFieldValueAsStringByName(JSON_SCHOOL_IDENTIFIER));
   }
 
+  public static void setRegistrationNumber(FilterDto filter, String identifier) {
+    set(filter, JSON_REGISTRATION_NUMBER, f -> f.getValueAsString(),
+        f -> f.setValueAsString(identifier));
+  }
+
+  public static String getRegistrationNumber(FilterDto filter) {
+    return get(filter, d -> d.getFieldValueAsStringByName(JSON_REGISTRATION_NUMBER));
+  }
+
+  public static void setFirstName(FilterDto filter, String identifier) {
+    set(filter, JSON_FIRST_NAME, f -> f.getValueAsString(), f -> f.setValueAsString(identifier));
+  }
+
+  public static String getFirstName(FilterDto filter) {
+    return get(filter, d -> d.getFieldValueAsStringByName(JSON_FIRST_NAME));
+  }
+
+  public static void setLastNames(FilterDto filter, String identifier) {
+    set(filter, JSON_LAST_NAMES, f -> f.getValueAsString(), f -> f.setValueAsString(identifier));
+  }
+
+  public static String getLastNames(FilterDto filter) {
+    return get(filter, d -> d.getFieldValueAsStringByName(JSON_LAST_NAMES));
+  }
+
+  public static void setArabicFirstName(FilterDto filter, String identifier) {
+    set(filter, JSON_ARABIC_FIRST_NAME, f -> f.getValueAsString(),
+        f -> f.setValueAsString(identifier));
+  }
+
+  public static String getArabicFirstName(FilterDto filter) {
+    return get(filter, d -> d.getFieldValueAsStringByName(JSON_ARABIC_FIRST_NAME));
+  }
+
+  public static void setArabicLastNames(FilterDto filter, String identifier) {
+    set(filter, JSON_ARABIC_LAST_NAMES, f -> f.getValueAsString(),
+        f -> f.setValueAsString(identifier));
+  }
+
+  public static String getArabicLastNames(FilterDto filter) {
+    return get(filter, d -> d.getFieldValueAsStringByName(JSON_ARABIC_LAST_NAMES));
+  }
+
   public static final String JSON_SCHOOL_IDENTIFIER = StudentDto.JSON_SCHOOL_IDENTIFIER;
+  public static final String JSON_REGISTRATION_NUMBER = StudentDto.JSON_REGISTRATION_NUMBER;
+  public static final String JSON_FIRST_NAME = StudentDto.JSON_FIRST_NAME;
+  public static final String JSON_LAST_NAMES = StudentDto.JSON_LAST_NAMES;
+  public static final String JSON_ARABIC_FIRST_NAME = StudentDto.JSON_ARABIC_FIRST_NAME;
+  public static final String JSON_ARABIC_LAST_NAMES = StudentDto.JSON_ARABIC_LAST_NAMES;
 }
