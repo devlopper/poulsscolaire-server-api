e31da6d0f296bac1d8b1dda205c4f9b19adb92b8
Christian Yao Komenan <c.komenan@budget.gouv.ci>
2024-12-03 21:57:11 +0000

    refactoring

--- src/main/java/org/cyk/system/poulsscolaire/server/api/accounting/FundingFilter.java
+++ src/main/java/org/cyk/system/poulsscolaire/server/api/accounting/FundingFilter.java
@@ -2,6 +2,7 @@ package org.cyk.system.poulsscolaire.server.api.accounting;
 
 import ci.gouv.dgbf.extension.server.service.api.AbstractIdentifiableFilter;
 import ci.gouv.dgbf.extension.server.service.api.request.FilterDto;
+import java.time.Month;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,6 +18,10 @@ public class FundingFilter extends AbstractIdentifiableFilter {
 
   String budgetIdentifier;
 
+  String departmentIdentifier;
+
+  Month month;
+
   public FundingFilter(FilterDto dto) {
     super(dto);
   }
@@ -27,14 +32,27 @@ public class FundingFilter extends AbstractIdentifiableFilter {
   protected void doInitialize(FilterDto filter) {
     super.doInitialize(filter);
     budgetIdentifier = getBudgetIdentifier(filter);
+    departmentIdentifier = getDepartmentIdentifier(filter);
+    month = getMonth(filter);
   }
 
   @Override
   protected void toDto(FilterDto filter) {
     super.toDto(filter);
     setBudgetIdentifier(filter, budgetIdentifier);
+    setDepartmentIdentifier(filter, departmentIdentifier);
+    setMonth(filter, month);
   }
-  
+
+  public static void setDepartmentIdentifier(FilterDto filter, String identifier) {
+    set(filter, JSON_DEPARTMENT_IDENTIFIER, f -> f.getValueAsString(),
+        f -> f.setValueAsString(identifier));
+  }
+
+  public static String getDepartmentIdentifier(FilterDto filter) {
+    return get(filter, d -> d.getFieldValueAsStringByName(JSON_DEPARTMENT_IDENTIFIER));
+  }
+
   public static void setBudgetIdentifier(FilterDto filter, String identifier) {
     set(filter, JSON_BUDGET_IDENTIFIER, f -> f.getValueAsString(),
         f -> f.setValueAsString(identifier));
@@ -44,5 +62,15 @@ public class FundingFilter extends AbstractIdentifiableFilter {
     return get(filter, d -> d.getFieldValueAsStringByName(JSON_BUDGET_IDENTIFIER));
   }
 
-  public static final String JSON_BUDGET_IDENTIFIER = FundingDto.JSON_BUDGET_IDENTIFIER; 
+  public static void setMonth(FilterDto filter, Month month) {
+    set(filter, JSON_MONTH, f -> f.getValueAsString(), f -> f.setValueAsEnum(month));
+  }
+
+  public static Month getMonth(FilterDto filter) {
+    return get(filter, d -> d.getFieldValueAsEnum(JSON_MONTH, Month.class));
+  }
+
+  public static final String JSON_BUDGET_IDENTIFIER = FundingDto.JSON_BUDGET_IDENTIFIER;
+  public static final String JSON_DEPARTMENT_IDENTIFIER = FundingDto.JSON_DEPARTMENT_IDENTIFIER;
+  public static final String JSON_MONTH = FundingDto.JSON_MONTH;
 }
